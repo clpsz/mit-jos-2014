f076a2949295f0e353fd39b4e924dad751b74a87
Zuo Pengpeng <pengclpsz@gmail.com>
2015-04-13 19:01:35 -0700

    Add code for exercise8_5

--- Lab1/2014-jos-Lab1/kern/init.c
+++ Lab1/2014-jos-Lab1/kern/init.c
@@ -38,6 +38,8 @@ i386_init(void)
         int x = 1, y = 3, z = 4;
     Lab1_exercise8_3:
         cprintf("x %d, y %x, z %d\n", x, y, z);
+    Lab1_exercise8_5:
+        cprintf("x=%d y=%d", 3);
     }
     {
         unsigned int i = 0x000a646c;
