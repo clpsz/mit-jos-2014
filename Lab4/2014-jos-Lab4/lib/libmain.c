26813e09e8debc65dda8b67c20474764af0b89fb
clpsz <pengclpsz@gmail.com>
2015-05-10 17:56:45 +0800

    Add code for lab4
    Already modify from lab1, lab2, lab3

---
+++ Lab4/2014-jos-Lab4/lib/libmain.c
@@ -0,0 +1,29 @@
+// Called from entry.S to get us going.
+// entry.S already took care of defining envs, pages, uvpd, and uvpt.
+
+#include <inc/lib.h>
+
+extern void umain(int argc, char **argv);
+
+const volatile struct Env *thisenv;
+const char *binaryname = "<unknown>";
+
+void
+libmain(int argc, char **argv)
+{
+	// set thisenv to point at our Env structure in envs[].
+	// LAB 3: Your code here.
+	envid_t i = sys_getenvid();
+    thisenv = &envs[ENVX(i)];
+
+	// save the name of the program so that panic() can use it
+	if (argc > 0)
+		binaryname = argv[0];
+
+	// call user main routine
+	umain(argc, argv);
+
+	// exit gracefully
+	exit();
+}
+
